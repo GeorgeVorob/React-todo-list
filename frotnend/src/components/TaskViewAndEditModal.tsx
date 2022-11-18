3887be740d6d8e54ed611c8ac49cd793a53ddf15
GeorgeVorob <54977627+GeorgeVorob@users.noreply.github.com>
2022-11-18 17:11:35 +0600

    Add modal window

--- frotnend/src/components/TaskViewAndEditModal.tsx
+++ frotnend/src/components/TaskViewAndEditModal.tsx
@@ -1,4 +1,5 @@
-import { Backdrop, Box, Fade, Modal, Typography } from "@mui/material";
+import { Backdrop, Box, Button, Fade, Modal, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 interface TaskViewAndEditModalProps {
     opened: boolean,
@@ -11,12 +12,46 @@ function TaskViewAndEditModal(props: TaskViewAndEditModalProps) {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 400,
+        width: 700,
         bgcolor: 'background.paper',
         border: '2px solid #000',
         boxShadow: 24,
         p: 4,
     };
+
+    const [editMode, setEditMode] = useState(false);
+
+    function OnModalClose() {
+        setEditMode(false);
+        props.closeCallback();
+    }
+
+    function OnEditStart() {
+        setEditMode(true);
+    }
+
+    function OnEditSubmit() {
+        setEditMode(false);
+    }
+
+    let taskData;
+    if (!editMode) {
+        taskData = (<>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+                Task description.
+            </Typography>
+            <Button onClick={OnEditStart}>Edit</Button>
+        </>)
+    }
+    else {
+        taskData = (
+            <>
+                <TextField sx={{ width: '100%', marginBottom: '10px' }} multiline id='new-desc' rows={5} ></TextField>
+                <Button onClick={OnEditSubmit}>Save</Button>
+                <Button onClick={() => setEditMode(false)}>Discard</Button>
+            </>
+        )
+    }
 
     return (
         <div>
@@ -24,7 +59,7 @@ function TaskViewAndEditModal(props: TaskViewAndEditModalProps) {
                 aria-labelledby="transition-modal-title"
                 aria-describedby="transition-modal-description"
                 open={props.opened}
-                onClose={props.closeCallback}
+                onClose={OnModalClose}
                 closeAfterTransition
                 BackdropComponent={Backdrop}
                 BackdropProps={{
@@ -36,9 +71,9 @@ function TaskViewAndEditModal(props: TaskViewAndEditModalProps) {
                         <Typography id="transition-modal-title" variant="h6" component="h2">
                             Text in a modal
                         </Typography>
-                        <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-                        </Typography>
+                        <p>Created 12.12.2012</p>
+                        <p>Status: completed</p>
+                        {taskData}
                     </Box>
                 </Fade>
             </Modal>
